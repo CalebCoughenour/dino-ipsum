d93d9b9ae961cc221402158b1402f67d1ec8e5a4
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-19 15:12:25 -0700

    add clearform function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+const clearForm = () => {
+  $('#paragraphs').text("");
+  $('$words').text("");
+};
+
+$('#dino-form').submit(function(e) {
+  e.preventDefault();
+  let paragraphs = $('#paragraphs').val();
+  let words = $('#wods').val();
+  clearForm();
+  
+});
